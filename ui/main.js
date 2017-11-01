7a197c441ff66345486c5337da2a2228272bd411
anisha1408 <31811636+anisha1408@users.noreply.github.com>
2017-11-01 12:14:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('nadi');
-img.onClick= function(){
-  img.style.marginLeft='100px';  
+nadi.onClick= function(){
+  nadi.style.marginLeft='100px';  
 };
