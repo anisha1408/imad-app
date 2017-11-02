abcb9b334f69832b15a9a457c1f4f8bebddf98a1
anisha1408 <31811636+anisha1408@users.noreply.github.com>
2017-11-02 09:34:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,15 @@
 var button= document.getElementById('counter');
-var counter=0;
-button.onClick=function(){
-    
-
-
-
-
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML= counter.toString();
+button.onClick = function() {
+    var request= new XMLHttpRequest();
+    request.onreadystatechange =  function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            if(request.status===200) {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://anishamohanty1998.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
